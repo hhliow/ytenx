c7c81fa805551da6b2d44bdd0d69431c2d1652f4
hhliow <hxliow@gmail.com>
2018-11-04 13:37:22 -0500

    Update urls.py

--- ytenx/dciangxkox/urls.py
+++ ytenx/dciangxkox/urls.py
@@ -4,9 +4,9 @@ from django.conf.urls import patterns
 urlpatterns = patterns('',
   (r'^$', 'ytenx.dciangxkox.views.dciangx_kox'),
   (r'^dzih/$', 'ytenx.dciangxkox.views.dzih_pieux'),
-  (r'^dzih/(.+)/$', 'ytenx.dciangxkox.views.dzih'),
+  (r'^dzih/(?P<ziox>.+)/$', 'ytenx.dciangxkox.views.dzih'),
   (r'^cjeng/$', 'ytenx.dciangxkox.views.cjeng_byo_pieux'),
-  (r'^cjeng/(.+)/$', 'ytenx.dciangxkox.views.cjeng_byo'),
+  (r'^cjeng/(?P<ziox>.+)/$', 'ytenx.dciangxkox.views.cjeng_byo'),
   (r'^yonh/$', 'ytenx.dciangxkox.views.yonh_box_pieux'),
-  (r'^yonh/(.+)/$', 'ytenx.dciangxkox.views.yonh_box'),
+  (r'^yonh/(?P<ziox>.+)/$', 'ytenx.dciangxkox.views.yonh_box'),
 )
