96a8b7bc82facf055a1c9394965564961faecf53
hhliow <hxliow@gmail.com>
2018-11-04 13:38:06 -0500

    Update urls.py

--- ytenx/tcenghyonhtsen/urls.py
+++ ytenx/tcenghyonhtsen/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import patterns
 
 urlpatterns = patterns('',
   (r'^sieux$', 'ytenx.tcenghyonhtsen.views.sieux_yonh_list_page'),
-  (r'^sieux/(\d{1,4})/$', 'ytenx.tcenghyonhtsen.views.sieux_yonh_page'),
+  (r'^sieux/(?P<ziox>\d{1,4})/$', 'ytenx.tcenghyonhtsen.views.sieux_yonh_page'),
   (r'^yonhmiuk$', 'ytenx.tcenghyonhtsen.views.yonh_miuk_list_page'),
-  (r'^yonhmiuk/(.+)/$', 'ytenx.tcenghyonhtsen.views.yonh_miuk_page'),
-  (r'^cio/(\d{1,1})/(\d{1,3})/$', 'ytenx.tcenghyonhtsen.views.cio_page'),
+  (r'^yonhmiuk/(?P<ziox>.+)/$', 'ytenx.tcenghyonhtsen.views.yonh_miuk_page'),
+  (r'^cio/(?P<ziox>\d{1,1})/(\d{1,3})/$', 'ytenx.tcenghyonhtsen.views.cio_page'),
   (r'^$', 'ytenx.tcenghyonhtsen.views.index_page'),
 )
