29a0e8e35129e2820b7c58990806a6433f5a44f0
hhliow <hxliow@gmail.com>
2018-11-04 12:53:02 -0500

    Update views.py

--- ytenx/dciangxkox/views.py
+++ ytenx/dciangxkox/views.py
@@ -7,10 +7,12 @@ from ytenx.helpers.paginator import Paginator
 from django.core.paginator import InvalidPage, EmptyPage
 from models import *
 
-def dciangx_kox(request, vertical):
-  return render_to_response('dciangxkox/dciangx_kox.html')
+def dciangx_kox(request, vertical=0):
+  return render_to_response('dciangxkox/dciangx_kox.html', {
+    'vertical': vertical,
+  })
 
-def dzih(request, id, vertical):
+def dzih(request, id, vertical=0):
   try:
     dzih = Dzih.objects.get(id = id)
   except:
@@ -18,9 +20,10 @@ def dzih(request, id, vertical):
 
   return render_to_response('dciangxkox/dzih.html', {
     'dzih': dzih,
+    'vertical': vertical,
   })
 
-def dzih_pieux(request, vertical):
+def dzih_pieux(request, vertical=0):
   yonh = request.GET.get('yonh')
   
   query = Q()
@@ -43,10 +46,11 @@ def dzih_pieux(request, vertical):
     'dzih_pieux': dzih_pieux,
     'cjeng_pieux': cjeng_pieux,
     'yonh_pieux': yonh_pieux,
+    'vertical': vertical,
   })
 
 @cache_page(60 * 60 * 24)
-def cjeng_byo_pieux(request, vertical):
+def cjeng_byo_pieux(request, vertical=0):
   cjeng_pieux = CjengByo.objects.all()
   paginator = Paginator(cjeng_pieux, 15)
   try:
@@ -56,10 +60,11 @@ def cjeng_byo_pieux(request, vertical):
   
   return render_to_response('dciangxkox/cjeng_byo_pieux.html', {
     'cjeng_pieux': cjeng_pieux,
+    'vertical': vertical,
   })
 
 @cache_page(60 * 60 * 24)
-def cjeng_byo(request, mjeng, vertical):
+def cjeng_byo(request, mjeng, vertical=0):
   try:
     cjeng = CjengByo.objects.get(mjeng = mjeng)
   except:
@@ -67,16 +72,18 @@ def cjeng_byo(request, mjeng, vertical):
 
   return render_to_response('dciangxkox/cjeng_byo.html', {
     'cjeng': cjeng,
+    'vertical': vertical,
   })
 
 @cache_page(60 * 60 * 24)
-def yonh_box_pieux(request, vertical):
+def yonh_box_pieux(request, vertical=0):
   return render_to_response('dciangxkox/yonh_box_pieux.html', {
     'yonh_pieux': YonhBox.objects.all(),
+    'vertical': vertical,
   })
 
 @cache_page(60 * 60 * 24)
-def yonh_box(request, mjeng, vertical):
+def yonh_box(request, mjeng, vertical=0):
   try:
     yonh = YonhBox.objects.get(mjeng = mjeng)
   except:
@@ -84,4 +91,5 @@ def yonh_box(request, mjeng, vertical):
 
   return render_to_response('dciangxkox/yonh_box.html', {
     'yonh': yonh,
+    'vertical': vertical,
   })
