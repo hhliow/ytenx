182a35c6f00e6c1277adb37f0e2007c6ca71cc5e
hhliow <hxliow@gmail.com>
2018-11-04 13:38:57 -0500

    Update urls.py

--- ytenx/tcenghyonhtsen/urls.py
+++ ytenx/tcenghyonhtsen/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
   (r'^sieux/(?P<ziox>\d{1,4})/$', 'ytenx.tcenghyonhtsen.views.sieux_yonh_page'),
   (r'^yonhmiuk$', 'ytenx.tcenghyonhtsen.views.yonh_miuk_list_page'),
   (r'^yonhmiuk/(?P<ziox>.+)/$', 'ytenx.tcenghyonhtsen.views.yonh_miuk_page'),
-  (r'^cio/(?P<ziox>\d{1,1})/(\d{1,3})/$', 'ytenx.tcenghyonhtsen.views.cio_page'),
+  (r'^cio/(?P<ziox>\d{1,1})/(?P<ziox2>\d{1,3})/$', 'ytenx.tcenghyonhtsen.views.cio_page'),
   (r'^$', 'ytenx.tcenghyonhtsen.views.index_page'),
 )
