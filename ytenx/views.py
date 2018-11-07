c60a7a817145641deb8e49c4d61a29fc7b4a4fd2
hhliow <hxliow@gmail.com>
2018-11-07 08:24:16 -0500

    Update views.py

--- ytenx/views.py
+++ ytenx/views.py
@@ -13,8 +13,8 @@ from dciangxkox.models import Dzih as DciangxKoxDzih
 def index_page(request):  
   if 'layout' in request.GET or 'layout' not in request.session: 
     request.session['layout'] = request.GET.get('layout', 'horizontal');
-    if (request.path):
-      return redirect(request.path)
+    if ('path' in request.GET):
+      return redirect(request.GET.path)
     return redirect('ytenx.views.index_page')
   return render(request, 'index.html')
 
